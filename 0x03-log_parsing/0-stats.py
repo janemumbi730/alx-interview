ab04e1ab906fcc57dd7bd179da831f6c85fda814
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2024-01-24 15:20:50 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python3
 """
-Task: 0. Log Parsing
-File: 0x06-log_parsing/0-stats.py
+Module 0-stats
 """
 from sys import stdin
 
 
-def printstats(file_size, status_codes):
-    """
-    This prints statistics at the beginning and every 10 lines
-    This will also be called on a Keyboard interruption
-    """
-    print("File size: " + str(file_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(code + ": " + str(status_codes[code]))
-
-
-line_num = 0
 file_size = 0
-status_code = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
+status_code = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+    }
 
-try:
-    for line in stdin:
-        line_num += 1
-        split_line = line.split()
 
-        if len(split_line) > 1:
-            file_size += int(split_line[-1])
+def print_file_size():
+    """Prints the file size and status codes"""
+    print("File size: {}".format(file_size))
 
-        if len(split_line) > 2 and split_line[-2].isnumeric():
-            status_code = split_line[-2]
-        else:
-            status_code = 0
+    for k, v in sorted(status_code.items()):
+        if v > 0:
+            print("{}: {}".format(k, v))
 
-        if status_code in status_codes.keys():
-            status_codes[status_code] += 1
 
-        if line_num % 10 == 0:
-            printstats(file_size, status_codes)
+if __name__ == "__main__":
+    """Entry point"""
 
-    printstats(file_size, status_codes)
+    try:
+        for k, in_put in enumerate(stdin, 1):
+            try:
+                data = in_put.split()
+                file_size += int(data[-1])
+                if data[-2] in status_code.keys():
+                    status_code[data[-2]] += 1
 
-except (KeyboardInterrupt):
-    printstats(file_size, status_codes)
-    raise
+            except ValueError:
+                pass
+
+            if not k % 10:
+                print_file_size()
+    except KeyboardInterrupt:
+        print_file_size()
+        raise
+    print_file_size()
